d6c58c7d36c76f02c48adc8aea94d1288e6f6522
kennethlng <ng.kennethlee@gmail.com>
2020-10-22 12:05:58 -0700

    Create meta.js

---
+++ src/constants/meta.js
@@ -0,0 +1,8 @@
+export const SLOGAN = "Quick and easy checklists for you and your friends";
+export const TITLE = `NMBO - ${SLOGAN}`
+export const DESCRIPTION = "Just copy the link and share it. No account or download needed. Free to use.";
+export const SIGN_IN_TITLE = "Log in | NMBO";
+export const SIGN_UP_TITLE = "Sign up | NMBO";
+export const ABOUT_TITLE = "About | NMBO";
+export const FAQS_TITLE = "FAQs | NMBO"
+export const PROJECT_TITLE = (projectTitle) => `${projectTitle} | NMBO`;
