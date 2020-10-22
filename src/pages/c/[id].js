d11557e43c1776cac1b3928035a9a41cae2ebc9e
kennethlng <ng.kennethlee@gmail.com>
2020-10-22 13:48:35 -0700

    Updated project page with SetProjectTitleInput and ProjectDoesntExistPlaceholder

--- src/pages/c/[id].js
+++ src/pages/c/[id].js
@@ -2,22 +2,43 @@ import App from '../../components/App'
 import { db } from '../../lib/firebase'
 import Head from 'next/head'
 import * as META from '../../constants/meta'
+import * as DB from '../../constants/db'
 import ProjectTasks from '../../components/ProjectTasks'
+import ProjectDoesntExistPlaceholder from '../../components/ProjectDoesntExistPlaceholder'
+import SetProjectTitleInput from '../../components/SetProjectTitleInput'
 
 export default function Project({ data }) {
-    if (data) {
-        return (
-            <App>
-                <Head>
-                    <title>{META.PROJECT_TITLE(data.title)}</title>
-                </Head>
-                {data.title}
-                <ProjectTasks/>
-            </App>
-        )
-    }
-
-    return <div>This project doesn't exist</div>
+    return (
+        <App>
+            <Head>
+                <title>{META.PROJECT_TITLE(data ? data.title : "🤔")}</title>
+            </Head>
+            <section className="hero">
+                <div className="hero-body">
+                    <div className="container">
+                        <div className="columns is-centered">
+                            <div className="column is-three-fifths is-two-thirds-tablet">
+                                {data ? (
+                                    <div>
+                                        <div className="block">
+                                            <SetProjectTitleInput
+                                                initialValue={data[DB.TITLE]}
+                                            />
+                                        </div>
+                                        <div className="block mb-6">
+                                            <ProjectTasks/>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <ProjectDoesntExistPlaceholder/>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </App>
+    )
 }
 
 export async function getServerSideProps(context) {
