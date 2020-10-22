07c7fb0721f10c628358845f6deed43f307c61c9
kennethlng <ng.kennethlee@gmail.com>
2020-10-22 13:47:59 -0700

    Created ProjectDoesntExistPlaceholder

---
+++ src/components/ProjectDoesntExistPlaceholder/index.js
@@ -0,0 +1,12 @@
+export default function ProjectDoesntExistPlaceholder() {
+    return (
+        <div className="block has-text-centered">
+            <h1 className="title is-1">
+                🤷‍♀️ 🤷 🤷‍♂️
+            </h1>
+            <h2 className="subtitle is-3">
+                This checklist doesn't exist
+            </h2>
+        </div>
+    )
+}
