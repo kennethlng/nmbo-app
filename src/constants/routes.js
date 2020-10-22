df70b44dc32d4780b1610594909cc158bf09f4f6
kennethlng <ng.kennethlee@gmail.com>
2020-10-22 11:52:28 -0700

    Added constants files for routes and social

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const HOME = '/';
+export const PROJECT = (id) => `/c/${id}`; 
+export const PROJECTS = '/p'
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const ABOUT = '/about';
+export const FAQS = '/faqs'; 
---
+++ src/constants/social.js
@@ -0,0 +1,2 @@
+export const EMAIL = "hello@nmbo.app";
+export const MESSENGER = "https://m.me/nmboapp";
