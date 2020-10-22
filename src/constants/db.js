80cb4c5b37d0e75bcdfffdffdca375926db5efec
kennethlng <ng.kennethlee@gmail.com>
2020-10-21 23:43:20 -0700

    Created db constants file

---
+++ src/constants/db.js
@@ -0,0 +1,17 @@
+module.exports = {
+    USERS: "users",
+    PROJECTS: "projects",
+    USER_PROJECTS: "user_projects",
+    PROJECT_TASKS: "project_tasks",
+    CREATED_ON: "created_on",
+    CREATED_BY: "created_by",
+    MODIFIED_ON: "modified_on",
+    MODIFIED_BY: "modified_by",
+    OPENED_ON: "opened_on",
+    TITLE: "title",
+    ID: "id",
+    VISIT_COUNTER: "visit_counter",
+    IS_HEARTED: "is_hearted",
+    IS_COMPLETED: "is_completed" 
+}
+
