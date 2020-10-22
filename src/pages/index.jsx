c71d6e1a385f21e35b12158fe96fe5e26ef4876c
kennethlng <ng.kennethlee@gmail.com>
2020-10-22 13:14:20 -0700

    Updated home page with RecentAuthUserProjectsMenu

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,49 @@
 import App from '../components/App'
 import Head from 'next/head'
 import * as META from '../constants/meta'
+import RecentAuthUserProjectsMenu from '../components/RecentAuthUserProjectsMenu'
+import { AuthUserContext } from '../components/Session'
+import { useContext } from 'react'
 
 export default function Home() {
+  const authUser = useContext(AuthUserContext);
+
   return (
     <App>
       <Head>
         <title>{META.TITLE}</title>
       </Head>
-      <p>Index Page</p>
+      {authUser ? (
+        <div>
+          <section className="hero">
+            <div className="hero-body">
+              <div className="container">
+                <div className="columns is-centered">
+                  <div className="column is-half">
+                    <div className="block my-6">
+                      <h1 className="title is-1 has-text-weight-bold">
+                        Make a checklist
+                      </h1>
+                      {/* <AddProject
+                        onSuccess={handleSuccess}
+                      /> */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="section">
+            <div className="container">
+              <div className="columns is-centered">
+                <div className="column is-half">
+                  <RecentAuthUserProjectsMenu/>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      ) : null}
     </App>
   )
 }
