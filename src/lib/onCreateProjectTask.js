c5b46c7c9bd1472af6c71e2ea62277cc76686da2
kennethlng <ng.kennethlee@gmail.com>
2020-10-23 00:13:40 -0700

    Removed firestore triggers, which is handled separately in nmbo-app-cloud-functions repo
    There are 2 separate Firebase libraries- one for client-side and one for node.js. Since this client-side repo uses only one node_modules folder, I can't use certain libraries that require the node.js firebase.

--- src/lib/onCreateProjectTask.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions'); 
-const DB = require('../constants/db');  
-
-exports.onCreateProjectTask = functions.firestore.document(`${DB.PROJECTS}/{projectId}/${DB.PROJECT_TASKS}/{taskId}`).onCreate((snapshot, context) => {
-    return snapshot.ref.update({
-        [DB.CREATED_ON]: snapshot.createTime
-    })
-})
