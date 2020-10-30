39e966f96ec62ad58ded1f43ec4cc174bcbe3e41
kennethlng <ng.kennethlee@gmail.com>
2020-10-30 09:05:41 -0700

    Updated pageTitle constants variable wiht signin and project page titles

--- src/constants/pageTitle.js
+++ src/constants/pageTitle.js
@@ -4,3 +4,4 @@ export const PRIVACY_POLICY = "Privacy Policy";
 export const TERMS_OF_SERVICE = "Terms of Service"; 
 export const SIGN_UP = "Sign Up";
 export const SIGN_IN = "Sign In"; 
+export const PROJECT = "Project";
