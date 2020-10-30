349e158a6c1cc57a0e4bd457da99ff22a6c3453b
kennethlng <ng.kennethlee@gmail.com>
2020-10-30 12:12:03 -0700

    Updated RecentAuthUserProjectsMenu with option to route to Checklists page to view all user_projects

--- src/components/RecentAuthUserProjectsMenu/index.js
+++ src/components/RecentAuthUserProjectsMenu/index.js
@@ -3,12 +3,16 @@ import { AuthUserContext } from '../Session';
 import UserProjectsMenuList from '../UserProjectsMenuList'; 
 import { UserProject } from '../../models/UserProject'; 
 import * as DB from '../../constants/db'; 
+import * as ROUTES from '../../constants/routes';
 import { db } from '../../lib/firebase';
+import { useRouter } from 'next/router'
 
 export default function RecentAuthUserProjectsMenu() {
     const authUser = useContext(AuthUserContext); 
+    const router = useRouter();
     const [userProjects, setUserProjects] = useState([]); 
     const [loading, setLoading] = useState(false); 
+    const limit = 10; 
 
     useEffect(() => {
         list(); 
@@ -17,7 +21,10 @@ export default function RecentAuthUserProjectsMenu() {
     const list = () => {
         setLoading(true); 
 
-        db.collection(DB.USERS).doc(authUser.uid).collection(DB.USER_PROJECTS).orderBy(DB.OPENED_ON, "desc").get()
+        db.collection(DB.USERS).doc(authUser.uid).collection(DB.USER_PROJECTS)
+        .orderBy(DB.OPENED_ON, "desc")
+        .limit(limit)
+        .get()
         .then(function(querySnapshot) {
             var items = []; 
             querySnapshot.forEach(function(doc) {
@@ -39,11 +46,33 @@ export default function RecentAuthUserProjectsMenu() {
             userProjects.length > 0 ? (
                 <aside className="menu">
                     <p className="menu-label">
-                        My Checklists
+                        Recent
                     </p>
                     <UserProjectsMenuList
                         userProjects={userProjects}
                     />
+                    {userProjects.length >= limit ? (
+                        <div className="menu-list">
+                            <a onClick={() => router.push(ROUTES.PROJECTS)}>
+                                <div className="level is-mobile has-text-link">
+                                    <div className="level-left">
+                                        <div className="level-item">
+                                            <p className="is-size-5">
+                                                See all my checklists
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="level-right">
+                                        <div className="level-item">
+                                            <span className="icon">
+                                                <i className="fas fa-arrow-right"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    ) : null}
                 </aside>
             ) : null 
         )
