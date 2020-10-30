480205cbcf51e82cbd10a4b7b37091664dce378c
kennethlng <ng.kennethlee@gmail.com>
2020-10-30 09:05:26 -0700

    Fixed issue with firebase not initializing server side due to window check

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,20 +4,18 @@ import 'firebase/auth';
 import 'firebase/firestore'; 
 import 'firebase/analytics'; 
 
-if (typeof window !== 'undefined' && !firebase.apps.length) {
+try {
     firebase.initializeApp(config)
+} catch (error) {
+    if (!/already exists/.test(error.message)) {
+        console.error('Firebase initialization error', error.stack);
+    }
+}
 
+if (typeof window !== 'undefined') {
     // To enable analytics
     if ('measurementId' in config) firebase.analytics(); 
 }
-
-// try {
-//     firebase.initializeApp(config)
-// } catch (error) {
-//     if (!/already exists/.test(error.message)) {
-//         console.error('Firebase initialization error', error.stack);
-//     }
-// }
 
 const auth = firebase.auth();
 const db = firebase.firestore(); 
