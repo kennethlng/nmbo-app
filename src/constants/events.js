9091adfe17ebfee7aab3329126fe219122698d34
kennethlng <ng.kennethlee@gmail.com>
2020-10-29 20:41:56 -0700

    Created constants file for events

---
+++ src/constants/events.js
@@ -0,0 +1,5 @@
+export const PAGE_VIEW = "page_view";
+export const LOGIN = "login";
+export const VIEW_ITEM = "view_item";
+export const VIEW_ITEM_LIST = "view_item_list";
+export const SIGN_UP = "sign_up"; 
