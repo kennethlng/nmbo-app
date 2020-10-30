c48ae92e1b147d5c3cd4e7448d3b7f2333a595af
kennethlng <ng.kennethlee@gmail.com>
2020-10-29 20:43:28 -0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,24 +16,25 @@ export default function MyApp({ Component, pageProps }) {
   const [authUser, setAuthUser] = useState(null); 
 
   useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged(function (authUser) {
-        if (authUser) {
-          setAuthUser(authUser)
-        } else {
-          setAuthUser(null);
+    // AuthStateChanged listener
+    const unsubscribe = auth.onAuthStateChanged(function (authUser) {
+      if (authUser) {
+        setAuthUser(authUser)
+      } else {
+        setAuthUser(null);
 
-          auth.signInAnonymously()
-            .catch(function (error) {
-              // Handle Errors here.
-              var errorCode = error.code;
-              var errorMessage = error.message;
-              // ...
-              console.log(errorMessage);
-            });
-        }
-      })
-      
-      return () => unsubscribe()
+        auth.signInAnonymously()
+          .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            console.log(errorMessage);
+          });
+      }
+    })
+
+    return () => unsubscribe()
   }, [])
 
   Router.events.on('routeChangeStart', (url) => {
