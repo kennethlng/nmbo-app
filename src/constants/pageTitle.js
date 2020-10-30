342936903ddc6a41d12e6e9ebaa8856a093731be
kennethlng <ng.kennethlee@gmail.com>
2020-10-29 20:42:24 -0700

    Created constants file for page titles

---
+++ src/constants/pageTitle.js
@@ -0,0 +1,6 @@
+export const HOME = "Home";
+export const ABOUT = "About"; 
+export const PRIVACY_POLICY = "Privacy Policy"; 
+export const TERMS_OF_SERVICE = "Terms of Service"; 
+export const SIGN_UP = "Sign Up";
+export const SIGN_IN = "Sign In"; 
