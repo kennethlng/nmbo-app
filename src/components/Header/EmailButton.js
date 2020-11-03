9ff931a2f073200b1fb4c631e085ab1cbdded6a1
kennethlng <ng.kennethlee@gmail.com>
2020-11-03 13:37:24 -0800

    Created header EmailButton component
    Includes logging analytics event

---
+++ src/components/Header/EmailButton.js
@@ -0,0 +1,21 @@
+import * as SOCIAL from '../../constants/social'
+import * as CONTENT_TYPE from '../../constants/contentType'
+import * as CONTENT_ID from '../../constants/contentId'
+import { firebase } from '../../lib/firebase'
+
+export default function EmailButton() {
+    const handleClick = () => {
+        firebase.analytics().logEvent('select_content', {
+            content_id: CONTENT_ID.HEADER_EMAIL_BUTTON,
+            content_type: CONTENT_TYPE.BUTTON
+        })
+    }
+
+    return (
+        <a href={`mailto:${SOCIAL.EMAIL}`} onClick={handleClick}>
+            <span className="icon is-large">
+                <i className="fas fa-envelope fa-lg"></i>
+            </span>
+        </a>
+    )
+}
