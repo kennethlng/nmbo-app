c4b1bdd82961cf3483d26ba8b21d7d4cf3bce6d2
kennethlng <ng.kennethlee@gmail.com>
2020-11-03 16:31:00 -0800

    Updated Checklists page to log page_view, view_item_list, and select_item events

--- src/pages/c.jsx
+++ src/pages/c.jsx
@@ -1,19 +1,39 @@
 import App from '../components/App'
 import Head from 'next/head'
-import { db } from '../lib/firebase'
+import { db, firebase } from '../lib/firebase'
 import { useContext, useEffect, useState } from 'react'
 import { AuthUserContext } from '../components/Session'
 import * as DB from '../constants/db'
 import * as META from '../constants/meta'
 import * as ROUTES from '../constants/routes'
+import * as PAGE_TITLE from '../constants/pageTitle'
+import * as LIST_ID from '../constants/listId'
+import * as LIST_NAME from '../constants/listName'
 import { UserProject } from '../models/UserProject'
-import UserProjectsMenuList from '../components/UserProjectsMenuList'; 
+import { useRouter } from 'next/router'
 
 export default function Projects() {
     const authUser = useContext(AuthUserContext);
+    const router = useRouter(); 
     const [userProjects, setUserProjects] = useState([]); 
 
     useEffect(() => {
+        // Log event for page view
+        firebase.analytics().logEvent('page_view', {
+            page_path: router.pathname,
+            page_title: PAGE_TITLE.PROJECTS,
+            page_location: window.location.href
+        })
+
+        // Log Google Analytics event for viewing item list
+        firebase.analytics().logEvent('view_item_list', {
+            item_list_id: LIST_ID.ALL_USER_PROJECTS,
+            item_list_name: LIST_NAME.ALL_USER_PROJECTS
+        })
+    }, [])
+
+    useEffect(() => {
+        // Fetch authUser's user_projects
         if (authUser) {
             db.collection(DB.USERS).doc(authUser.uid).collection(DB.USER_PROJECTS)
             .orderBy(DB.TITLE)
@@ -31,6 +51,23 @@ export default function Projects() {
             })
         }
     }, [authUser])
+
+    const handleRowClick = (userProject) => {
+        // Log Google Analytics event for selecting item
+        firebase.analytics().logEvent('select_item', {
+            items: [{
+                item_id: userProject[DB.ID],
+                item_name: userProject[DB.TITLE],
+                item_list_name: LIST_NAME.ALL_USER_PROJECTS,
+                item_list_id: LIST_ID.ALL_USER_PROJECTS
+            }],
+            item_list_name: LIST_NAME.ALL_USER_PROJECTS,
+            item_list_id: LIST_ID.ALL_USER_PROJECTS
+        })
+
+        // Route to project page
+        router.push(ROUTES.PROJECT(userProject[DB.ID]))
+    }
 
     return (
         <App>
@@ -50,9 +87,17 @@ export default function Projects() {
                                 Checklists
                             </h1>
                             <aside className="menu">
-                                <UserProjectsMenuList
-                                    userProjects={userProjects}
-                                />
+                                <ul className="menu-list">
+                                    {userProjects.map(userProject => (
+                                        <li key={userProject[DB.ID]}>
+                                            <a onClick={() => handleRowClick(userProject)}>
+                                                <p className="is-size-5">
+                                                    {userProject[DB.TITLE]}
+                                                </p>
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
                             </aside>
                         </div>
                     </div>
