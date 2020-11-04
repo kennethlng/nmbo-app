0388d0d0dfcbbaea360f4767f1f635e3e1a7947d
kennethlng <ng.kennethlee@gmail.com>
2020-11-03 16:30:25 -0800

    Created constants file for user_projects lists

---
+++ src/constants/listId.js
@@ -0,0 +1,2 @@
+export const ALL_USER_PROJECTS = "all_user_projects";
+export const RECENT_USER_PROJECTS = "recent_user_projects";
---
+++ src/constants/listName.js
@@ -0,0 +1,2 @@
+export const ALL_USER_PROJECTS = "All user projects";
+export const RECENT_USER_PROJECTS = "Recent user projects";
