2d1e1c67770b0ea2e823037be7cd3df2dffdeb96
kennethlng <ng.kennethlee@gmail.com>
2020-11-04 22:08:17 -0800

    Updated social with feedback form link

--- src/constants/social.js
+++ src/constants/social.js
@@ -1,3 +1,4 @@
 export const EMAIL = "hello@nmbo.app";
 export const MESSENGER = "https://m.me/nmboapp";
 export const TWITTER = "https://twitter.com/nmboapp";
+export const FEEDBACK = "https://forms.gle/qpu2hbUWFgNozDAQ8"; 
