0a1936ea7939413b22e76ae4319af8f534a51a09
kennethlng <ng.kennethlee@gmail.com>
2020-11-05 16:02:58 -0800

    Updated ProjectTasks to log event for view_item_list

--- src/components/ProjectTasks/index.js
+++ src/components/ProjectTasks/index.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import ProjectTasksList from '../ProjectTasksList'; 
 import { Task } from '../../models/Task'; 
 import * as DB from '../../constants/db';
-import { db } from '../../lib/firebase';
+import * as LIST_ID from '../../constants/listId';
+import * as LIST_NAME from '../../constants/listName'; 
+import { db, firebase } from '../../lib/firebase';
 import { useRouter } from 'next/router'
 import NoTasksPlaceholder from './NoTasksPlaceholder'; 
 
@@ -12,6 +14,12 @@ export default function ProjectTasks() {
     const [loading, setLoading] = useState(false); 
 
     useEffect(() => {
+        // Log Google Analytics event for view_item_list
+        firebase.analytics().logEvent('view_item_list', {
+            item_list_id: LIST_ID.PROJECT_TASKS,
+            item_list_name: LIST_NAME.PROJECT_TASKS
+        })
+
         setLoading(true); 
 
         var unsubscribe = db.collection(DB.PROJECTS).doc(router.query.id).collection(DB.PROJECT_TASKS).onSnapshot(function(querySnapshot) {
