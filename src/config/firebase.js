3c59ef0f15ff53a1403577e0dc061f183e415317
kennethlng <ng.kennethlee@gmail.com>
2020-11-09 17:08:12 -0800

    Updated firebase config with dev and prod configs

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-exports.config = {
+exports.prodConfig = {
     apiKey: "AIzaSyBr8xHaUys9dW5aAqVJS6wbjynUg7g1FeA",
     authDomain: "chop-chop-97e18.firebaseapp.com",
     databaseURL: "https://chop-chop-97e18.firebaseio.com",
@@ -7,4 +7,15 @@ exports.config = {
     messagingSenderId: "110931751070",
     appId: "1:110931751070:web:650acf5bc709ac436b8b8f",
     measurementId: "G-FJ011MPN0F"
+}
+
+exports.devConfig = {
+    apiKey: "AIzaSyAXBgZen545ADarwuR98dMUig7u3rziVtM",
+    authDomain: "nmbo-dev.firebaseapp.com",
+    databaseURL: "https://nmbo-dev.firebaseio.com",
+    projectId: "nmbo-dev",
+    storageBucket: "nmbo-dev.appspot.com",
+    messagingSenderId: "543496163940",
+    appId: "1:543496163940:web:a5ba199f30e82d7d024571",
+    measurementId: "G-VP65QFTH6F"
 }
