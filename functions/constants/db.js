44864259fb4aaa068b291dd7ac29a30b4e2dcf6a
kennethlng <ng.kennethlee@gmail.com>
2020-11-09 18:19:23 -0800

    Merged Cloud Functions trigger functions into this project

---
+++ functions/constants/db.js
@@ -0,0 +1,18 @@
+module.exports = {
+    USERS: "users",
+    PROJECTS: "projects",
+    USER_PROJECTS: "user_projects",
+    PROJECT_TASKS: "project_tasks",
+    CREATED_ON: "created_on",
+    CREATED_BY: "created_by",
+    MODIFIED_ON: "modified_on",
+    MODIFIED_BY: "modified_by",
+    OPENED_ON: "opened_on",
+    TITLE: "title",
+    ID: "id",
+    VISIT_COUNTER: "visit_counter",
+    NUM_TASKS_TOTAL: "num_tasks_total",
+    NUM_TASKS_COMPLETED: "num_tasks_completed",
+    IS_COMPLETED: "is_completed",
+    IS_HEARTED: "is_hearted"
+}
