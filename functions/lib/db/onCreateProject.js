fd66270f686f922c769d40e2e9ace224a5e6ada7
kennethlng <ng.kennethlee@gmail.com>
2020-11-14 22:22:06 -0800

    Added onCreateProject to set createTime for project doc and user_project docs

--- functions/lib/db/onCreateProject.js
+++ functions/lib/db/onCreateProject.js
@@ -1,12 +1,28 @@
 const functions = require('firebase-functions'); 
 const CONSTANTS = require('../../constants'); 
+const { db } = require('../admin'); 
 
 exports.onCreateProject = functions.firestore.document(`${CONSTANTS.DB.PROJECTS}/{projectId}`).onCreate((snapshot, context) => {
-    const data = snapshot.data(); 
+    let batch = db.batch(); 
 
-    return snapshot.ref.set({
-        [CONSTANTS.DB.CREATED_ON]: snapshot.createTime,
-        [CONSTANTS.DB.MODIFIED_ON]: snapshot.createTime,
-        [CONSTANTS.DB.MODIFIED_BY]: data[CONSTANTS.DB.CREATED_BY]
+    batch.set(snapshot.ref, {
+        [CONSTANTS.DB.CREATED_ON]: snapshot.createTime
     }, { merge: true })
+
+    return db.collectionGroup(CONSTANTS.DB.USER_PROJECTS).where(CONSTANTS.DB.ID, '==', snapshot.ref.id).get()
+        .then(function(querySnapshot) {
+            if (querySnapshot.empty) {
+                console.log("No matching documents");
+                return null;
+            }
+
+            // Track the created_on date for each user_project
+            querySnapshot.forEach(function(doc) {
+                batch.set(doc.ref, {
+                    [CONSTANTS.DB.CREATED_ON]: snapshot.createTime
+                }, { merge: true })
+            })
+
+            return batch.commit(); 
+        })
 })
