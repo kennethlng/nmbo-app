b111ebb37d918d35e78b5d14fee54883ca609fda
kennethlng <ng.kennethlee@gmail.com>
2020-11-16 23:03:43 -0800

    Update onCreateProject.js

--- functions/lib/db/onCreateProject.js
+++ functions/lib/db/onCreateProject.js
@@ -3,26 +3,36 @@ const CONSTANTS = require('../../constants');
 const { db } = require('../admin'); 
 
 exports.onCreateProject = functions.firestore.document(`${CONSTANTS.DB.PROJECTS}/{projectId}`).onCreate((snapshot, context) => {
+    const data = snapshot.data(); 
     let batch = db.batch(); 
 
+    // Set the create time for the project doc
     batch.set(snapshot.ref, {
         [CONSTANTS.DB.CREATED_ON]: snapshot.createTime
     }, { merge: true })
 
-    return db.collectionGroup(CONSTANTS.DB.USER_PROJECTS).where(CONSTANTS.DB.ID, '==', snapshot.ref.id).get()
-        .then(function(querySnapshot) {
-            if (querySnapshot.empty) {
-                console.log("No matching documents");
-                return null;
-            }
+    // Set the create time for the user_project doc for the user who created the project 
+    let userProjRef = db.collection(CONSTANTS.DB.USERS).doc(data[CONSTANTS.DB.CREATED_BY]).collection(CONSTANTS.DB.USER_PROJECTS).doc(snapshot.id); 
+    batch.set(userProjRef, {
+        [CONSTANTS.DB.CREATED_ON]: snapshot.createTime
+    }, { merge: true })
 
-            // Track the created_on date for each user_project
-            querySnapshot.forEach(function(doc) {
-                batch.set(doc.ref, {
-                    [CONSTANTS.DB.CREATED_ON]: snapshot.createTime
-                }, { merge: true })
-            })
+    return batch.commit()
 
-            return batch.commit(); 
-        })
+    // return db.collectionGroup(CONSTANTS.DB.USER_PROJECTS).where(CONSTANTS.DB.ID, '==', snapshot.ref.id).get()
+    //     .then(function(querySnapshot) {
+    //         if (querySnapshot.empty) {
+    //             console.log("No matching documents");
+    //             return null;
+    //         }
+
+    //         // Track the created_on date for each user_project
+    //         querySnapshot.forEach(function(doc) {
+    //             batch.set(doc.ref, {
+    //                 [CONSTANTS.DB.CREATED_ON]: snapshot.createTime
+    //             }, { merge: true })
+    //         })
+
+    //         return batch.commit(); 
+    //     })
 })
