20e6a3a598741cbaaf8917f879449ecd8a52dad0
kennethlng <ng.kennethlee@gmail.com>
2020-11-17 09:52:13 -0800

    Moved signin and signup buttons to account dropdown

--- src/components/Header/AccoutNavbarItemDropdown.js
+++ src/components/Header/AccoutNavbarItemDropdown.js
@@ -1,18 +1,28 @@
-import SignOutNavbarItem from './SignOutNavbarItem'
 import ProjectsNavbarItem from './ProjectsNavbarItem'
+import SignOutNavbarItem from './SignOutNavbarItem'
+import SignUpNavbarItem from './SignUpNavbarItem'
+import SignInNavbarItem from './SignInNavbarItem'
+import { AuthUserContext } from '../Session'
+import { useRouter } from 'next/router'
 
 export default function AccountNavbarItemDropdown() {
+    const authUser = React.useContext(AuthUserContext); 
+    const router = useRouter(); 
+
     return (
         <div className="navbar-item has-dropdown is-hoverable">
             <a className="navbar-link has-background-white-ter">
-                <span className="icon">
-                    <i className="fas fa-user" aria-hidden></i>
-                </span>
+                😗
             </a>
             <div className="navbar-dropdown is-right is-boxed">
                 <ProjectsNavbarItem/>
                 <hr className="navbar-divider"></hr>
-                <SignOutNavbarItem/>
+                {authUser && authUser.isAnonymous ? (
+                    <div>
+                        <SignUpNavbarItem/>
+                        <SignInNavbarItem/>
+                    </div>
+                ) : <SignOutNavbarItem/>}
             </div>
         </div>
     )
