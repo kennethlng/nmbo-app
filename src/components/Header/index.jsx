65c4cddd48a13acb74d05e9fa33f71ca212dc6d8
kennethlng <ng.kennethlee@gmail.com>
2020-11-17 09:52:43 -0800

    Added account dropdown and hid navbar-end items when in signin and signup page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,16 +2,12 @@ import * as React from 'react'
 import LogoNavBarItemButton from './LogoNavBarItemButton'
 import AboutNavBarItemButton from './AboutNavBarItemButton'
 import FeedbackNavBarItemButton from './FeedbackNavBarItemButton'
-import SignInButton from './SignInButton'
-import SignUpButton from './SignUpButton'
-import { AuthUserContext } from '../Session'
+import AccountNavbarItemDropdown from './AccoutNavbarItemDropdown'
 import { useRouter } from 'next/router'
 import * as ROUTES from '../../constants/routes'
-import AccountNavbarItemDropdown from './AccoutNavbarItemDropdown'
 
 const Header = () => {
-  const authUser = React.useContext(AuthUserContext); 
-  const router = useRouter(); 
+  const router = useRouter();
   const [menuIsActive, setMenuIsActive] = React.useState(false); 
 
   return (
@@ -32,20 +28,13 @@ const Header = () => {
             
           </div>
 
-          <div className="navbar-end">
-            <AboutNavBarItemButton />
-            <FeedbackNavBarItemButton />
-            {router.pathname === ROUTES.SIGN_IN || router.pathname === ROUTES.SIGN_UP ? null
-              : authUser && authUser.isAnonymous ? (
-                <div className="navbar-item">
-                  <div className="buttons">
-                    <SignUpButton />
-                    <SignInButton />
-                  </div>
-                </div>
-              ) : <AccountNavbarItemDropdown/>
-            }
-          </div>
+          {router.pathname !== ROUTES.SIGN_IN && router.pathname !== ROUTES.SIGN_UP ? (
+            <div className="navbar-end">
+              <AboutNavBarItemButton />
+              <FeedbackNavBarItemButton />
+              <AccountNavbarItemDropdown />
+            </div>
+          ) : null}
         </div>
       </nav>
     </header>
