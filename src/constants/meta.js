1d34b5c9cbac8f645ada95c8b8efa7c0ee0a7cec
kennethlng <ng.kennethlee@gmail.com>
2020-11-18 15:18:35 -0800

    Updated slogan meta

--- src/constants/meta.js
+++ src/constants/meta.js
@@ -1,4 +1,4 @@
-export const SLOGAN = "The fastest way to make a checklist with friends";
+export const SLOGAN = "Drop in, drop out checklists";
 export const TITLE = `NMBO - ${SLOGAN}`
 export const DESCRIPTION = "Create a checklist and share the link, and anyone can start contributing. No account or download needed. Free to use.";
 export const SIGN_IN_TITLE = "Log in | NMBO";
