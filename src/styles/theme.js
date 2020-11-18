67b314e3ea2bb96c182124d124f43f43cb77facd
kennethlng <ng.kennethlee@gmail.com>
2020-11-18 14:26:27 -0800

    Update theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -5,6 +5,7 @@ const theme = createMuiTheme({
     palette: {
         primary: {
             main: '#FF8370',
+            contrastText: 'white'
         },
         secondary: {
             main: '#19857b',
@@ -16,6 +17,26 @@ const theme = createMuiTheme({
             default: '#fff',
         },
     },
+    typography: {
+        h1: {
+            fontWeight: 700
+        },
+        h3: {
+            fontWeight: 700,
+            color: '#4a4a4a'
+        },
+        fontFamily: [
+            'Assistant',
+            'sans-serif'
+        ].join(','),
+        button: {
+            textTransform: 'capitalize',
+            fontWeight: 700
+        }
+    },
+    shape: {
+        borderRadius: 12
+    }
 })
 
 export default theme; 
