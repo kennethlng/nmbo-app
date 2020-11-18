49761cb6c6897aba56f1c8f6c5981258ed8ba7dd
kennethlng <ng.kennethlee@gmail.com>
2020-11-17 16:57:01 -0800

    Update AccoutNavbarItemDropdown.js

--- src/components/Header/AccoutNavbarItemDropdown.js
+++ src/components/Header/AccoutNavbarItemDropdown.js
@@ -3,16 +3,16 @@ import SignOutNavbarItem from './SignOutNavbarItem'
 import SignUpNavbarItem from './SignUpNavbarItem'
 import SignInNavbarItem from './SignInNavbarItem'
 import { AuthUserContext } from '../Session'
-import { useRouter } from 'next/router'
 
 export default function AccountNavbarItemDropdown() {
     const authUser = React.useContext(AuthUserContext); 
-    const router = useRouter(); 
 
     return (
         <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link has-background-white-ter">
-                😗
+            <a className="navbar-link is-arrowless">
+                <span className="icon">
+                    <i className="fas fa-cog" aria-hidden></i>
+                </span>
             </a>
             <div className="navbar-dropdown is-right is-boxed">
                 <ProjectsNavbarItem/>
