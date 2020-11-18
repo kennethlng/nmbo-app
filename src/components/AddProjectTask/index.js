999a5ee1aeb2203c60557f94e91cb10ed89031ac
kennethlng <ng.kennethlee@gmail.com>
2020-11-18 12:48:22 -0800

    Updated AddProjectTask to use projectId prop instead of router query id

--- src/components/AddProjectTask/index.js
+++ src/components/AddProjectTask/index.js
@@ -12,23 +12,18 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
-import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles(theme => ({
-    input: {
-        marginTop: theme.spacing(3)
-    }
-}))
 
 export default function AddProjectTask(props) {
-    const { onSuccess, onError } = props; 
+    const { projectId, onSuccess, onError } = props; 
     const router = useRouter(); 
-    const classes = useStyles(); 
     const authUser = useContext(AuthUserContext); 
     const [title, setTitle] = useState(''); 
     const [loading, setLoading] = useState(false); 
 
     const add = () => {
+        console.log(projectId)
+        return;
+
         // Log event for button click 
         firebase.analytics().logEvent('select_content', {
             content_id: CONTENT_ID.ADD_TASK_BUTTON,
@@ -41,7 +36,7 @@ export default function AddProjectTask(props) {
 
         setLoading(true);
 
-        db.collection(DB.PROJECTS).doc(router.query.id).collection(DB.PROJECT_TASKS).add({
+        db.collection(DB.PROJECTS).doc(projectId).collection(DB.PROJECT_TASKS).add({
             [DB.TITLE]: title,
             [DB.CREATED_BY]: authUser.uid,
             [DB.MODIFIED_BY]: authUser.uid,
@@ -81,7 +76,7 @@ export default function AddProjectTask(props) {
     }
 
     return (
-        <FormControl variant="outlined" fullWidth className={classes.input}>
+        <FormControl variant="outlined" fullWidth>
             <InputLabel htmlFor="outlined-adornment-password">New task</InputLabel>
             <OutlinedInput
                 id="outlined-adornment-password"
