feb49e23bbe4b6554d1ec8d0ed6d64623b26f2be
kennethlng <ng.kennethlee@gmail.com>
2020-11-19 19:14:13 -0800

    Updated logo to use 100% height

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,6 +3,6 @@ import * as META from '../../constants/meta'
 
 export default function Logo() {
     return (
-        <img src={META.LOGO} width="100%"></img>
+        <img src={META.LOGO} height="100%"></img>
     )
 }
