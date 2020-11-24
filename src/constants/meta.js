b1d6f6bcfa8600a69392e80daf5b5571ed04c5c2
kennethlng <ng.kennethlee@gmail.com>
2020-11-23 17:26:43 -0800

    Update meta.js

--- src/constants/meta.js
+++ src/constants/meta.js
@@ -1,4 +1,4 @@
-export const SLOGAN = "Drop in, drop out checklists";
+export const SLOGAN = "The fastest way to make a checklist with friends";
 export const TITLE = `NMBO - ${SLOGAN}`
 export const DESCRIPTION = "Create a checklist and share the link, and anyone can start contributing. No account or download needed. Free to use.";
 export const SIGN_IN_TITLE = "Log in | NMBO";
@@ -8,7 +8,7 @@ export const FAQS_TITLE = "FAQs | NMBO";
 export const PRIVACY_POLICY_TITLE = "Privacy Policy | NMBO";
 export const TERMS_OF_SERVICE_TITLE = "Terms of Service | NMBO";
 export const PROJECTS_TITLE = "Checklists | NMBO"; 
-export const PROJECT_TITLE = (projectTitle) => `${projectTitle} | See my checklist on NMBO`;
+export const PROJECT_TITLE = (projectTitle) => `${projectTitle} | NMBO`;
 export const IMAGE = "https://i.imgur.com/gQ3azKV.png";
 export const URL = "https://nmbo.app";
 export const CHECKLIST_DOESNT_EXIST = "Checklist doesn't exist | NMBO";
