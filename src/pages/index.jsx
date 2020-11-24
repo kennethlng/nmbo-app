b63ad95a5a628bf2e2e533b60ab2b4ee4c13e100
kennethlng <ng.kennethlee@gmail.com>
2020-11-24 11:14:31 -0800

    Updated home page as a landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,17 +4,52 @@ import * as META from '../constants/meta'
 import * as ROUTES from '../constants/routes'
 import * as EVENTS from '../constants/events'
 import * as PAGE_TITLE from '../constants/pageTitle'
-import { AuthUserContext } from '../components/Session'
 import { useContext, useEffect } from 'react'
 import AddProject from '../components/AddProject'
 import { useRouter } from 'next/router'
 import { firebase } from '../lib/firebase'
 import RecentAuthUserProjects from '../components/RecentAuthUserProjects'
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(3)
+  },
+  feature: {
+    // marginBottom: theme.spacing(4)
+  }
+}))
+
+const features = [
+  {
+    emoji: "💌",
+    title: "Share the link to share the checklist.",
+    description: "Creating and sharing a checklist is as easy as sending a link. Start a checklist and share the link with your friends on any of your favorite messaging apps."
+  },
+  {
+    emoji: "🥳🏖",
+    title: "Jump in anytime, anywhere.",
+    description: "Since NMBO lives on the web, you can open a checklist from any browser on any device."
+  },
+  {
+    emoji: "🥸",
+    title: "No account needed. And it's free.",
+    description: "NMBO is completely free to use. Join a checklist or create one, even if you don't have an account."
+  },
+  {
+    emoji: "👷‍♀️👨‍🌾🧑‍🚀",
+    title: "Get stuff done together.",
+    description: "Invite as many people as you want to your checklist. Projects are more fun with other people."
+  }
+]
 
 export default function Home() {
-  const authUser = useContext(AuthUserContext);
   const router = useRouter(); 
+  const classes = useStyles(); 
 
   const handleSuccess = (id) => router.push(ROUTES.PROJECT(id))
 
@@ -26,6 +61,24 @@ export default function Home() {
     })
   }, [])
 
+  const Feature = (props) => {
+    const { emoji, title, description} = props;
+
+    return (
+      <div className={classes.feature}>
+        <Typography variant="h4">
+          {emoji}
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body1" paragraph>
+          {description}
+        </Typography>
+      </div>
+    )
+}
+
   return (
     <App>
       <Head>
@@ -36,16 +89,39 @@ export default function Home() {
         <meta property="twitter:url" content={META.URL}/>
         <meta property="twitter:title" content={META.TITLE}/>
       </Head>
-      <Grid container spacing={6}>
-        <Grid item xs={12}>
-          <AddProject
-            onSuccess={handleSuccess}
-          />
+      <Container maxWidth="md">
+        <Grid container spacing={10} >
+          <Grid container item xs={12} spacing={1} alignItems="flex-start">
+            <Grid item>
+              <Typography variant="h3" component="h1">
+                NMBO is the fastest way to make a checklist with friends.
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <AddProject
+                onSuccess={handleSuccess}
+              />
+            </Grid>
+          </Grid>
+          <Grid container item xs={12} spacing={4}>
+            {features.map((feature, index) => (
+              <Grid
+                key={index}
+                md={3}
+                sm={6}
+                xs={12}
+                item
+              >
+                <Feature
+                  emoji={feature.emoji}
+                  title={feature.title}
+                  description={feature.description}
+                />
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <RecentAuthUserProjects/>
-        </Grid>
-      </Grid>
+      </Container>
     </App>
   )
 }
