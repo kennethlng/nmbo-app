91288684db0dcb8ac3c397063a7508c6c7a0ce4b
kennethlng <ng.kennethlee@gmail.com>
2020-11-23 16:41:13 -0800

    Created TwitterButton component

---
+++ src/components/TwitterButton/index.js
@@ -0,0 +1,21 @@
+import * as CONTENT_TYPE from '../../constants/contentType'
+import * as CONTENT_ID from '../../constants/contentId'
+import * as SOCIAL from '../../constants/social'
+import { firebase } from '../../lib/firebase'
+import IconButton from '@material-ui/core/IconButton'
+import Icon from '@material-ui/core/Icon'
+
+export default function TwitterButton() {
+    const handleClick = () => {
+        firebase.analytics().logEvent('select_content', {
+            content_id: CONTENT_ID.FOOTER_TWITTER_BUTTON,
+            content_type: CONTENT_TYPE.BUTTON
+        })
+    }
+
+    return (
+        <IconButton onClick={handleClick}>
+            <Icon className="fa fa-twitter" />
+        </IconButton>
+    )
+}
