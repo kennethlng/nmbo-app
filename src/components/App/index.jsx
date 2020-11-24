8db069a77183039d4a0fa18862bbbd28d60fd3b0
kennethlng <ng.kennethlee@gmail.com>
2020-11-23 20:58:06 -0800

    Updated App.js to show backdrop while route is loading

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Header from '../Header'
 import Sider from '../Sider'
 import { makeStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import Container from '@material-ui/core/Container'
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { AppStateContext } from '../AppState'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,15 +16,23 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     paddingTop: theme.spacing(6)
   },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
 }))
 
 const App = ({ children }) => {
   const classes = useStyles(); 
+  const appState = useContext(AppStateContext); 
 
   return (
     <main className={classes.root}>
       <Header />
       <Sider/>
+      <Backdrop className={classes.backdrop} open={appState.routeLoading}>
+        <CircularProgress/>
+      </Backdrop>
       <div className={classes.content}>
         <Toolbar/>
         <Container maxWidth="sm">
