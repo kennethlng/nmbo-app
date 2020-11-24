8fcff21c6bc36eaedce814bda8250f3a23df22f3
kennethlng <ng.kennethlee@gmail.com>
2020-11-24 13:52:06 -0800

    Create ArchiveMenuItem.js

---
+++ src/components/Header/ArchiveMenuItem.js
@@ -0,0 +1,29 @@
+import { useRouter } from 'next/router'
+import MenuItem from '@material-ui/core/MenuItem'
+import * as ROUTES from '../../constants/routes'
+import * as CONTENT_ID from '../../constants/contentId'
+import * as CONTENT_TYPE from '../../constants/contentType'
+import { firebase } from '../../lib/firebase'
+
+export default function ArchiveMenuItem() {
+    const router = useRouter(); 
+
+    const handleClick = () => {
+        // Log analytics event
+        firebase.analytics().logEvent('select_content', {
+            content_id: CONTENT_ID.HEADER_PROJECTS_BUTTON,
+            content_type: CONTENT_TYPE.BUTTON
+        })
+
+        // Push route
+        router.push(ROUTES.PROJECTS);
+    }
+
+    return (
+        <MenuItem
+            onClick={handleClick}
+        >
+            🗓 Archive
+        </MenuItem>
+    )
+}
