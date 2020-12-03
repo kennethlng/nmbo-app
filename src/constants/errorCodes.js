727a4386b5cde33c332a3a647896d501651b4b0e
kennethlng <ng.kennethlee@gmail.com>
2020-12-02 20:20:07 -0800

    Created errorCodes file and included Firebase Authentication error codes

---
+++ src/constants/errorCodes.js
@@ -0,0 +1,3 @@
+export const CREDENTIAL_ALREADY_IN_USE = "auth/credential-already-in-use"; 
+export const EMAIL_ALREADY_IN_USE = "auth/email-already-in-use"; 
+export const ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL = "auth/account-exists-with-different-credential";
