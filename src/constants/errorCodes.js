1046d730f2c92358dfb1da6e118fd6f592ed2915
kennethlng <ng.kennethlee@gmail.com>
2020-12-03 16:32:28 -0800

    Update errorCodes.js

--- src/constants/errorCodes.js
+++ src/constants/errorCodes.js
@@ -1,3 +1,7 @@
 export const CREDENTIAL_ALREADY_IN_USE = "auth/credential-already-in-use"; 
 export const EMAIL_ALREADY_IN_USE = "auth/email-already-in-use"; 
 export const ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL = "auth/account-exists-with-different-credential";
+export const PROVIDER_ALREADY_LINKED = "auth/provider-already-linked";
+export const USER_NOT_FOUND = "auth/user-not-found"; 
+export const WEAK_PASSWORD = "auth/weak-password";
+export const WRONG_PASSWORD = "auth/wrong-password"; 
