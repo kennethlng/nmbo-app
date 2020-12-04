9a1cc3c07314cf8b25b90de1c5e92e1b2207b49e
kennethlng <ng.kennethlee@gmail.com>
2020-12-03 16:39:10 -0800

    Streamlined signup page to handle login success and error

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,144 +1,80 @@
 import App from '../components/App'; 
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import * as ROUTES from '../constants/routes'; 
 import * as META from '../constants/meta'; 
-import * as METHODS from '../constants/methods';
-import * as TOAST from '../constants/toast'
-import * as EVENTS from '../constants/events'
 import * as PAGE_TITLE from '../constants/pageTitle'
-import * as CONTENT_TYPE from '../constants/contentType'
-import * as CONTENT_ID from '../constants/contentId'
+import * as ERROR_CODES from '../constants/errorCodes' 
 import { useRouter } from 'next/router';
-import { auth, firebase } from '../lib/firebase'
+import { firebase } from '../lib/firebase'
 import Head from 'next/head'
-import * as EmailValidator from 'email-validator'
-import { toast } from 'react-toastify'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button'; 
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container'
-import Link from '@material-ui/core/Link';
-import Divider from '@material-ui/core/Divider';
 import SignUpWithGoogleButton from '../components/SignUpWithGoogleButton';
 import SignUpWithFacebookButton from '../components/SignUpWithFacebookButton'; 
+import SignUpWithEmailAndPasswordForm from '../components/SignUpWithEmailAndPasswordForm';
+import SignInLink from '../components/SignInLink';
+import { AppStateContext } from '../components/AppState';
 
 const useStyles = makeStyles(theme => ({
-    form: {
-        width: '100%',
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(2, 0, 2)
+    body: {
+        marginTop: theme.spacing(3),
     }
 }))
 
 export default function SignUp() {
     const router = useRouter();
     const classes = useStyles(); 
-    const [email, setEmail] = useState(''); 
-    const [password, setPassword] = useState(''); 
-    const [loading, setLoading] = useState(false);
-    const [emailHelp, setEmailHelp] = useState(''); 
-    const [passwordHelp, setPasswordHelp] = useState(''); 
+    const appState = useContext(AppStateContext); 
 
     useEffect(() => {
         // Log event for page view 
-        firebase.analytics().logEvent(EVENTS.PAGE_VIEW, {
+        firebase.analytics().logEvent('page_view', {
             page_path: router.pathname,
             page_title: PAGE_TITLE.SIGN_UP,
             page_location: window.location.href
         })
     }, [])
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); 
+    const handleSuccess = (result) => {
+        var user = result.user;
 
-        // Log event for sign up button click 
-        firebase.analytics().logEvent('select_content', {
-            content_id: CONTENT_ID.SIGN_UP_PAGE_SIGN_UP_BUTTON,
-            content_type: CONTENT_TYPE.BUTTON
-        })
+        console.log("Account linking success: ", user);
 
-        if (loading) return;
+        // Snackbar
+        appState.setSnackbarMessage("You're signed in!");
+        appState.setSnackbarOpen(true); 
 
-        // Validate email
-        if (!EmailValidator.validate(email)) {
-            setEmailHelp('Please enter a valid email');
-            return 
+        // Push to home route
+        router.push(ROUTES.HOME); 
+    }
+
+    const handleError = (error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log("Error linking anonymous account: ", error)
+
+        // Snackbar
+        switch (errorCode) {
+            case ERROR_CODES.EMAIL_ALREADY_IN_USE:
+                appState.setSnackbarMessage("This email address is already in use.");
+                break;
+            case ERROR_CODES.CREDENTIAL_ALREADY_IN_USE: 
+                appState.setSnackbarMessage("This email address is already in use.");
+                break; 
+            case ERROR_CODES.WEAK_PASSWORD:
+                appState.setSnackbarMessage("Password should be at least 6 characters.")
+                break;
+            case ERROR_CODES.PROVIDER_ALREADY_LINKED:
+                appState.setSnackbarMessage("You are already logged in.");
+                break;
+            default:
+                appState.setSnackbarMessage("Oops! Something went wrong.");
         }
-
-        setLoading(true); 
-
-        // Link email and password
-        var credential = firebase.auth.EmailAuthProvider.credential(email, password);
-        auth.currentUser.linkWithCredential(credential)
-        .then(function(usercred) {
-            var user = usercred.user;
-            console.log("Account linking success", user);
-
-            // Analytics: Log sign-up event
-            firebase.analytics().logEvent(EVENTS.SIGN_UP, {
-                method: METHODS.PASSWORD
-            })
-
-            setLoading(false); 
-
-            toast('Account created!', {
-                autoClose: TOAST.autoClose,
-                hideProgressBar: true
-            })
-
-            // Push route
-            router.push(ROUTES.HOME)
-        }).catch(function (error) {
-            // console.log("Account linking error", error);
-            var errorMessage = error.message; 
-            var errorCode = error.code;
-
-            // Log event for error code and message
-            firebase.analytics().logEvent(EVENTS.SIGN_UP_ERROR, {
-                error_code: errorCode,
-                error_message: errorMessage
-            })
-
-            setPasswordHelp(errorMessage); 
-
-            // toast('Something went wrong!', {
-            //     autoClose: TOAST.autoClose,
-            //     hideProgressBar: true
-            // })
-
-            setLoading(false); 
-        });
-    }
-
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-        setEmailHelp('');
-    }
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-        setPasswordHelp(''); 
-    }
-
-    const handleSignInClick = () => {
-        // Log event for button click 
-        firebase.analytics().logEvent('select_content', {
-            content_id: CONTENT_ID.SIGN_UP_PAGE_SIGN_IN_BUTTON,
-            content_type: CONTENT_TYPE.BUTTON
-        })
-
-        // Push route
-        router.push(ROUTES.SIGN_IN)
-    }
-
-    const handleEnterKeyPress = (e) => {
-        if (e.key === "Enter") {
-            handleSubmit();
-        }
+        appState.setSnackbarOpen(true); 
     }
 
     return (
@@ -152,77 +88,42 @@ export default function SignUp() {
                 <meta property="twitter:title" content={META.SIGN_UP_TITLE} />
             </Head>
             <Container maxWidth="sm">
-                <Typography component="h1" variant="h3" paragraph>
+                <Typography component="h1" variant="h3" align="center">
                     Create an account
                 </Typography>
-                <Typography component="h3" variant="h5">
+                <Typography component="h3" variant="subtitle1" align="center">
                     Sync your checklists anywhere you go.
                 </Typography>
-                <form className={classes.form} onSubmit={handleSubmit}>
-                    <TextField
-                        variant="outlined"
-                        required
-                        margin="normal"
-                        fullWidth
-                        label="Email"
-                        name="email"
-                        autoComplete="email"
-                        disabled={loading}
-                        helperText={emailHelp}
-                        error={emailHelp === '' ? false : true}
-                        onChange={handleEmailChange}
-                    />
-                    <TextField
-                        variant="outlined"
-                        required
-                        margin="normal"
-                        fullWidth
-                        type="password"
-                        label="Password"
-                        name="password"
-                        autoComplete="password"
-                        disabled={loading}
-                        helperText={passwordHelp}
-                        error={passwordHelp === '' ? false : true}
-                        onChange={handlePasswordChange}
-                    />
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            <Button
-                                className={classes.submit}
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                                size="large"
-                                fullWidth
-                                disableElevation
-                                disableRipple
-                                disabled={loading}
-                            >
-                                Create Account
-                            </Button>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant="subtitle1" align="center">
-                                or
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <SignUpWithGoogleButton/>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <SignUpWithFacebookButton/>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Divider/>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Link variant="body2" onClick={handleSignInClick}>
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
+                <Grid container spacing={3} className={classes.body}>
+                    <Grid item xs={12}>
+                        <SignUpWithGoogleButton
+                            onSuccess={handleSuccess}
+                            onError={handleError}
+                        />
                     </Grid>
-                </form>
+                    {/* <Grid item xs={12}>
+                        <SignUpWithFacebookButton
+                            onSuccess={handleSuccess}
+                            onError={handleError}
+                        />
+                    </Grid> */}
+                    <Grid item xs={12}>
+                        <Typography variant="subtitle1" align="center">
+                            or
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <SignUpWithEmailAndPasswordForm
+                            onSuccess={handleSuccess}
+                            onError={handleError}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography align="center">
+                            <SignInLink/>
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Container>
         </App>
     )
