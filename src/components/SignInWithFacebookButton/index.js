2f1dbab065f7b1ca37da63f10a735b989c5ac971
kennethlng <ng.kennethlee@gmail.com>
2020-12-03 16:35:57 -0800

    Cleaned up SignInWithFacebookButton with only Button and onSuccess and onError prop calls

--- src/components/SignInWithFacebookButton/index.js
+++ src/components/SignInWithFacebookButton/index.js
@@ -5,26 +5,19 @@ import * as CONTENT_ID from '../../constants/contentId';
 import * as CONTENT_TYPE from '../../constants/contentType'; 
 import * as EVENTS from '../../constants/events'; 
 import * as METHODS from '../../constants/methods'; 
-import * as SNACKBAR from '../../constants/snackbar';
-import * as ROUTES from '../../constants/routes'; 
-import * as ERROR_CODES from '../../constants/errorCodes';
 import { useState } from 'react';
-import Snackbar from '@material-ui/core/Snackbar'; 
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import { useRouter } from 'next/router'; 
 
-export default function SignInWithFacebookButton() {
-    const router = useRouter(); 
+export default function SignInWithFacebookButton(props) {
+    const { onSuccess, onError } = props; 
     const [loading, setLoading] = useState(false);
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [snackbarMessage, setSnackbarMessage] = useState(''); 
 
     const handleClick = () => {
         firebase.analytics().logEvent('select_content', {
             content_id: CONTENT_ID.SIGN_IN_WITH_FACEBOOK_BUTTON,
             content_type: CONTENT_TYPE.BUTTON
         })
+
+        if (loading) return; 
 
         signIn(); 
     }
@@ -36,88 +29,41 @@ export default function SignInWithFacebookButton() {
 
         firebase.auth().signInWithPopup(provider)
             .then(function (result) {
-                var token = result.credential.accessToken;
-                var user = result.user;
-
                 // Log event for successful login 
                 firebase.analytics().logEvent(EVENTS.LOGIN, {
                     method: METHODS.FACEBOOK
                 })
 
-                // Snackbar
-                setSnackbarOpen(true); 
-                setSnackbarMessage("You're signed in!");
-
                 setLoading(false); 
 
-                // Push route
-                router.push(ROUTES.HOME);
+                onSuccess(result); 
             })
             .catch(function (error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-
-                console.log("Error signing in with Facebook: ", error)
-
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
-
                 // Log event for sign in error
                 firebase.analytics().logEvent(EVENTS.LOGIN_ERROR, {
                     error_code: errorCode,
-                    error_message: errorMessage
+                    error_message: errorMessage,
+                    method: METHODS.FACEBOOK
                 })
 
-                // Snackbar
-                switch (errorCode) {
-                    case ERROR_CODES.ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL:
-                        setSnackbarMessage("An account already exists with this email.")
-                        break;
-                    default:
-                        setSnackbarMessage("Oops! Something went wrong. Please try again.");
-                }
-
-                setSnackbarOpen(true); 
-
                 setLoading(false); 
+
+                onError(error); 
             });
     }
 
     return (
-        <div>
-            <Button
-                onClick={handleClick}
-                variant="contained"
-                disableElevation
-                disableRipple
-                fullWidth
-                disabled={loading}
-                size="large"
-                startIcon={<Icon className="fab fa-facebook-f" fontSize="small" />}
-            >
-                Continue with Facebook
-            </Button>
-            <Snackbar
-                anchorOrigin={{
-                    vertical: SNACKBAR.VERTICAL_ANCHOR,
-                    horizontal: SNACKBAR.HORIZONTAL_ANCHOR
-                }}
-                open={snackbarOpen}
-                autoHideDuration={SNACKBAR.AUTO_HIDE_DURATION}
-                onClose={() => setSnackbarOpen(false)}
-                message={snackbarMessage}
-                action={
-                    <React.Fragment>
-                        <IconButton size="small" color="inherit" onClick={() => setSnackbarOpen(false)}>
-                            <CloseIcon />
-                        </IconButton>
-                    </React.Fragment>
-                }
-            />
-        </div>
+        <Button
+            onClick={handleClick}
+            variant="contained"
+            disableElevation
+            disableRipple
+            fullWidth
+            disabled={loading}
+            size="large"
+            startIcon={<Icon className="fab fa-facebook-f" fontSize="small" />}
+        >
+            Continue with Facebook
+        </Button>
     )
 }
