75c796316b0dae866f39e727c9e6e3e93f1bb79c
kennethlng <ng.kennethlee@gmail.com>
2020-12-06 15:36:43 -0800

    Created displayName constants file for adjectives and avatars

---
+++ functions/constants/displayName.js
@@ -0,0 +1,37 @@
+module.exports = {
+    adjectives: [
+        'Sus', 
+        'Greedy', 
+        'Moody', 
+        'Happy', 
+        'Super',
+        'Hotheaded',
+        'Crying',
+        'Chill',
+        'Lit',
+        'Hangry',
+        'Rich',
+        'Raging',
+        'Hot',
+        'Smokin'
+    ],
+    avatars: [
+        "Santa",
+        "Giraffe",
+        "Grandma",
+        "Grandpa",
+        "Doggie",
+        "Cat",
+        "Boomer",
+        "Bear",
+        "Wizard",
+        "Lover",
+        "Alien",
+        "Pirate",
+        "Bot",
+        "Pumpkin",
+        "Poopoo",
+        "Ninja",
+        "Baby"
+    ]
+}
