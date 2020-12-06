798e70fc956541e173b2b342ebac36bc0396d367
kennethlng <ng.kennethlee@gmail.com>
2020-12-06 15:37:47 -0800

    Created onCreateAuthUser to handle creating new display name (if there isn't any) and for creating corresponding user document in Firestore

---
+++ functions/lib/auth/onCreateAuthUser.js
@@ -0,0 +1,29 @@
+const functions = require('firebase-functions');
+const CONSTANTS = require('../../constants'); 
+const { db, auth } = require('../admin'); 
+const displayNameHelpers = require('../../helpers/displayName'); 
+
+function createUser(id, displayName, photoURL) {
+    return db.collection(CONSTANTS.DB.USERS).doc(id).set({
+        [CONSTANTS.DB.DISPLAY_NAME]: displayName,
+        [CONSTANTS.DB.PHOTO_URL]: photoURL
+    })
+}
+
+exports.onCreateAuthUser = functions.auth.user().onCreate(user => {
+    // If the user doesn't have a display name
+    if (!user.displayName) {
+        // Create a random display name 
+        const newName = displayNameHelpers.generateRandomDisplayName(); 
+        return auth.updateUser(user.uid, {
+            displayName: newName
+        })
+        .then((userRecord) => {
+            // Create the corresponding user doc in Firestore
+            createUser(user.uid, newName, user.photoURL); 
+        })
+    } else {
+        // Create the corresponding user doc in Firestore
+        createUser(user.uid, user.displayName, user.photoURL);  
+    }
+})
