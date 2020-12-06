4c34148790d71d89c3fc7c4fc75e9f44437b0ff1
kennethlng <ng.kennethlee@gmail.com>
2020-12-06 15:37:53 -0800

    Create onDeleteAuthUser.js

---
+++ functions/lib/auth/onDeleteAuthUser.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const CONSTANTS = require('../../constants'); 
+const { db } = require('../admin'); 
+
+exports.onDeleteAuthUser = functions.auth.user().onDelete(user => {
+    let batch = db.batch(); 
+
+    // Delete the corresponding user document
+    let userRef = db.collection(CONSTANTS.DB.USERS).doc(user.uid); 
+    batch.delete(userRef); 
+
+    // Delete all corresponding project_user docs
+    return db.collectionGroup(CONSTANTS.DB.PROJECT_USERS).where(CONSTANTS.DB.ID, '==', user.uid).get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                batch.delete(doc.ref); 
+            })
+
+            return batch.commit(); 
+        })
+})
