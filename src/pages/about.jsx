6bb28ebaf6c059be438564fbc20700010813e70d
kennethlng <ng.kennethlee@gmail.com>
2020-12-08 16:31:16 -0800

    Created about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,125 @@
+import App from '../components/App'
+import Head from 'next/head'
+import * as META from '../constants/meta'
+import * as ROUTES from '../constants/routes'
+import * as EVENTS from '../constants/events'
+import * as PAGE_TITLE from '../constants/pageTitle'
+import { useEffect } from 'react'
+import AddProject from '../components/AddProject'
+import { useRouter } from 'next/router'
+import { firebase } from '../lib/firebase'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(3)
+  }
+}))
+
+const features = [
+  {
+    emoji: "💌",
+    title: "Share the link to share the checklist.",
+    description: "Creating and sharing a checklist is as easy as sending a link. Start a checklist and share the link with your friends on any of your favorite messaging apps."
+  },
+  {
+    emoji: "🥸",
+    title: "No account needed. And it's free.",
+    description: "NMBO is completely free to use. Join a checklist or create one, even if you don't have an account."
+  },
+  {
+    emoji: "🥳🏖",
+    title: "Jump in anytime, anywhere.",
+    description: "Since NMBO lives on the web, you can open a checklist from any browser on any device."
+  },
+  {
+    emoji: "👷‍♀️👨‍🌾🧑‍🚀",
+    title: "Get stuff done together.",
+    description: "Invite as many people as you want to your checklist. Projects are more fun with other people."
+  }
+]
+
+export default function About() {
+  const router = useRouter();
+  const classes = useStyles();
+
+  const handleSuccess = (id) => router.push(ROUTES.PROJECT(id))
+
+  useEffect(() => {
+    firebase.analytics().logEvent(EVENTS.PAGE_VIEW, {
+      page_path: router.pathname,
+      page_title: PAGE_TITLE.ABOUT,
+      page_location: window.location.href
+    })
+  }, [])
+
+  const Feature = (props) => {
+    const { emoji, title, description } = props;
+
+    return (
+      <div className={classes.feature}>
+        <Typography variant="h4">
+          {emoji}
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body1" paragraph>
+          {description}
+        </Typography>
+      </div>
+    )
+  }
+
+  return (
+    <App>
+      <Head>
+        <title>{META.ABOUT_TITLE}</title>
+        <meta name="title" content={META.ABOUT_TITLE} />
+        <meta property="og:url" content={META.URL + ROUTES.ABOUT} />
+        <meta property="og:title" content={META.ABOUT_TITLE} />
+        <meta property="twitter:url" content={META.URL + ROUTES.ABOUT} />
+        <meta property="twitter:title" content={META.ABOUT_TITLE} />
+      </Head>
+      <Container maxWidth="md">
+        <Grid container spacing={10} >
+          <Grid container item xs={12} spacing={1} alignItems="flex-start">
+            <Grid item>
+              <Typography variant="h3" component="h1">
+                NMBO is the fastest way to make a checklist with friends.
+              </Typography>
+              <Typography variant="subtitle1" component="h3">
+                Create a checklist and share the link, and anyone can start contributing.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={8} md={4}>
+              <AddProject
+                onSuccess={handleSuccess}
+              />
+            </Grid>
+          </Grid>
+          <Grid container item xs={12} spacing={4}>
+            {features.map((feature, index) => (
+              <Grid
+                key={index}
+                md={3}
+                sm={6}
+                xs={12}
+                item
+              >
+                <Feature
+                  emoji={feature.emoji}
+                  title={feature.title}
+                  description={feature.description}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Container>
+    </App>
+  )
+}
