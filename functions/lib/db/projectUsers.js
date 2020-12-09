2ca0324dceda785c0ed327f38839148c32aba4a3
kennethlng <ng.kennethlee@gmail.com>
2020-12-08 16:07:25 -0800

    Created projectUsers module with updateProjectUsersByUserId function

---
+++ functions/lib/db/projectUsers.js
@@ -0,0 +1,17 @@
+const { db } = require('../admin'); 
+const CONSTANTS = require('../../constants'); 
+
+exports.updateProjectUsersByUserId = function(userId, obj) {
+    return db.collectionGroup(CONSTANTS.DB.PROJECT_USERS).where(CONSTANTS.DB.ID, '==', userId).get()
+        .then(function (querySnapshot) {
+            if (querySnapshot.empty) return null;
+
+            let batch = db.batch();
+
+            querySnapshot.forEach(function (doc) {
+                batch.update(doc.ref, obj)
+            });
+
+            return batch.commit();
+        })
+}
