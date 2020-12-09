518c02c93992d4844e2bb55dece499cb68137bac
kennethlng <ng.kennethlee@gmail.com>
2020-12-08 16:27:52 -0800

    Updated AccountButton with UserAvatar and displayName

--- src/components/Header/AccountButton.js
+++ src/components/Header/AccountButton.js
@@ -1,4 +1,5 @@
 import Button from '@material-ui/core/Button'; 
+import IconButton from '@material-ui/core/IconButton'; 
 import Menu from '@material-ui/core/Menu';
 import Divider from '@material-ui/core/Divider';
 import Avatar from '@material-ui/core/Avatar'; 
@@ -9,26 +10,26 @@ import { useContext, useEffect, useState } from 'react';
 import { AuthUserContext } from '../Session'
 import SignOutMenuItem from './SignOutMenuItem'
 import SignInMenuItem from './SignInMenuItem'
-import ArchiveMenuItem from './ArchiveMenuItem';
-import { getDisplayNameEmojiAvatar } from '../../lib/getDisplayNameEmojiAvatar';
 import { useRouter } from 'next/router';
 import * as DB from '../../constants/db';
 import * as ROUTES from '../../constants/routes';
 import * as CONTENT_ID from '../../constants/contentId'; 
 import * as CONTENT_TYPE from '../../constants/contentType'; 
 import { firebase, db } from '../../lib/firebase'; 
+import UserAvatar from '../UserAvatar'; 
+import InputBase from '@material-ui/core/InputBase';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 const useStyles = makeStyles(theme => ({
-    avatar: {
-        width: theme.spacing(3),
-        height: theme.spacing(3),
-        backgroundColor: 'transparent'
-    },
     text: {
         padding: theme.spacing(2)
     },
     anonymous: {
         color: theme.palette.secondary.main
+    },
+    displayName: {
+        fontWeight: 800
     }
 }))
 
@@ -77,23 +78,16 @@ export default function AccountButton() {
 
     return (
         <div>
-            <Button
+            <IconButton
                 onClick={handleClick}
-                disableElevation
-                disableRipple
-                color={authUser && authUser.isAnonymous ? "secondary" : "default"}
-                startIcon={(
-                    <Avatar
-                        className={classes.avatar}
-                        alt={displayName}
-                        src={photoURL}
-                    >
-                        {getDisplayNameEmojiAvatar(displayName)}
-                    </Avatar>
-                )}
+                edge="end"
+                color="inherit"
             >
-                {displayName}
-            </Button>
+                <UserAvatar
+                    displayName={displayName}
+                    photoURL={photoURL}
+                />
+            </IconButton>
             <Menu
                 anchorEl={anchorEl}
                 anchorOrigin={{
@@ -109,32 +103,44 @@ export default function AccountButton() {
                 onClose={handleClose}
                 onClick={handleClose}
             >
-                {authUser && authUser.isAnonymous ? (
-                    <div>
-                        <div className={classes.text}>
-                            <Typography  variant="body1" display="block" gutterBottom>
-                                You are <strong className={classes.anonymous}>anonymous</strong>. 
-                            </Typography>
-                            <Typography variant="body2" gutterBottom>
-                                Checklists you create or open will only be remembered on this device.
-                            </Typography>
-                            <Typography variant="body2">
-                                <Link component="button" variant="body2" onClick={handleSignUp}>Create an account</Link> to save your checklists and access them anywhere.
-                            </Typography>
-                        </div>
-                        {/* <Divider/> 
-                        <ArchiveMenuItem/> */}
-                        <Divider/> 
-                        {/* <SignUpMenuItem/> */}
+                <div>
+                    <div className={classes.text}>
+                        <FormControl>
+                            <InputBase
+                                className={classes.displayName}
+                                readOnly
+                                value={displayName}
+                                startAdornment={
+                                    <InputAdornment position="start">
+                                        <UserAvatar
+                                            displayName={displayName}
+                                            photoURL={photoURL}
+                                        />
+                                    </InputAdornment>
+                                }
+                            />
+                        </FormControl>
+                        {authUser && authUser.isAnonymous ? (
+                            <div>
+                                <Typography variant="body1" display="block" gutterBottom>
+                                    You are <strong className={classes.anonymous}>anonymous {displayName}</strong>.
+                           </Typography>
+                                <Typography variant="body2" gutterBottom>
+                                    This is how you will appear to others in a checklist.
+                           </Typography>
+                                <Typography variant="body2" gutterBottom color="error">
+                                    Checklists you create or open will only be remembered on this device. <Link component="button" variant="body2" onClick={handleSignUp}><strong>Create an account</strong></Link> to save your checklists and access them anywhere.
+                           </Typography>
+                            </div> 
+                        ) : null}
+                    </div>
+                    <Divider/> 
+                    {authUser && authUser.isAnonymous ? (
                         <SignInMenuItem/>
-                    </div>
-                ) : (
-                    <div>
-                        {/* <ArchiveMenuItem/>
-                        <Divider/> */}
+                    ) : (
                         <SignOutMenuItem/>
-                    </div>
-                )}
+                    )}
+                </div>
             </Menu>
         </div>
     )
